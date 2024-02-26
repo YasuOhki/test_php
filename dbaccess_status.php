c6bc106695a0468ea7310bcb0948de228f0502fe
Yasuhiro Ohki <>
2024-02-26 12:49:41 +0900

    Task.1 ・登園/降園に関するデータベースの更新処理の実装に成功

--- dbaccess_status.php
+++ dbaccess_status.php
@@ -10,30 +10,16 @@
     require('dbaccess.php');
     $status = filter_input(INPUT_GET, 'status', FILTER_SANITIZE_NUMBER_INT);
     $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-    $sql = "UPDATE students SET status = ? WHERE id = ?";
+    $sql = "UPDATE students SET status = " . $status . " " . " WHERE id = " . $id;
     $stmt = $db->prepare($sql);
-    var_dump($stmt);
-    //exit();
-
-    $stmt->bindValue(1, $status, PDO::PARAM_INT);
-    $stmt->bindValue(2, $id, PDO::PARAM_INT);
-    var_dump($status);
-    var_dump($id);
-    exit();
     $success = $stmt->execute();
-    var_dump($success);
-    exit();
 
     if (!$success) {
-      var_dump($success);
-      exit();
       echo 'データ更新に失敗しました';
       die($db->error);
     }
     else {
-      var_dump($success);
-      exit();
-      echo 'データが更新されました';
+      // do nothing
     }
   ?>
   <div class='return'><a href="/test_php/index.php">戻る</a></div>
